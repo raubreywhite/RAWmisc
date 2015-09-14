7218d88c8a7992e7796225656cb8c641df12bbb4
Richard White <r.aubrey.white@gmail.com>
2015-09-14 09:56:53 +0200

    Updating

--- R/knitr.R
+++ R/knitr.R
@@ -38,12 +38,47 @@ RMDToHTMLPandoc <- function(inFile="", outFile="", tocDepth=2){
 #' If pandoc is available, it uses pandoc (and hence bibliography/citations)
 #' Otherwise uses knitr and no bibliography/citations
 RmdToHTML <- function(inFile="",outFile="", tocDepth=2){
+  rstudio.pandoc.installed <- system('"C:/Program Files/RStudio/bin/pandoc/pandoc" -v')==0
+  if(rstudio.pandoc.installed){
+    Sys.setenv(RSTUDIO_PANDOC="C:/Program Files/RStudio/bin/pandoc") 
+  }
   pandoc.installed <- system('pandoc -v')==0
-  if(pandoc.installed){
+  if(pandoc.installed | rstudio.pandoc.installed){
     RMDToHTMLPandoc(inFile=inFile,outFile=outFile, tocDepth=tocDepth)
   } else {
     RMDToHTMLKnitr(inFile=inFile,outFile=outFile, tocDepth=tocDepth)
   }
 }
 
+#' Uses preset CSS to knit html report
+#' Also defaults to base64 inline images
+#' CSS file taken from Max Gordon (http://gforge.se/packages/)
+#' If pandoc is available, it uses pandoc (and hence bibliography/citations)
+#' Otherwise uses knitr and no bibliography/citations
+RmdToDOCX <- function(inFile="",outFile="", tocDepth=2){
+  rstudio.pandoc.installed <- system('"C:/Program Files/RStudio/bin/pandoc/pandoc" -v')==0
+  if(rstudio.pandoc.installed){
+    Sys.setenv(RSTUDIO_PANDOC="C:/Program Files/RStudio/bin/pandoc")
+  }
+  pandoc.installed <- system('pandoc -v')==0
+  if(pandoc.installed | rstudio.pandoc.installed){
+    outFile <- str_split(outFile,"/") %>%
+      unlist
+    if(length(outFile)==1){
+      outDir <- getwd()
+    } else {
+      outDir <- file.path(getwd(),outFile[-length(outFile)])
+      outFile <- outFile[length(outFile)]
+      
+    }
+    
+    rmarkdown::render(
+      input=inFile,
+      output_file=outFile,
+      output_dir=outDir,
+      output_format=docx_document(toc=TRUE,toc_depth=tocDepth))
+  } else {
+    stop("pandoc not installed")
+  }
+}
 
