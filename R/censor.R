0bdb9c495b35bad658927aa405d703d6c806f7bd
Richard White <r.aubrey.white@gmail.com>
2016-07-25 10:59:42 +0200

    Update censor.R

--- R/censor.R
+++ R/censor.R
@@ -1,24 +1,28 @@
 Censor <- function(n,d=NULL,boundaries=NULL){
   set.seed(4)
   
-  index <- !is.na(n) & n >= 3
+  index <- n >= 3
+  index[is.na(index)] <- FALSE
   new <- n
   new[index] <- new[index] + sample(c(-3:3),sum(index),replace=TRUE)
   if(!is.null(boundaries)){
     if(!is.list(boundaries)) boundaries <- list(boundaries)
     for(i in 1:length(boundaries)){
       index <- new >= boundaries[[i]] & n < boundaries[[i]]
+      index[is.na(index)] <- FALSE
       new[index] <- boundaries[[i]][index] - 1
     }
   }
   n <- new
   
-  index <- !is.na(n) & n < 3
+  index <- n < 3
+  index[is.na(index)] <- FALSE
   n[index] <- 0
   
   if(!is.null(d)){
     if(length(n) != length(d)) stop("n is different length to d")
-    index <- !is.na(d) & d < 10
+    index <- d < 10
+    index[is.na(index)] <- FALSE
     n[index] <- 0
   }
   
