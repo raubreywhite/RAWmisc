6078725c8f358e2e943d97e415ce43a489e533ff
Richard White <r.aubrey.white@gmail.com>
2016-12-20 12:16:30 +0100

    Update and rename emails.R to dashboards.R

---
+++ R/dashboards.R
@@ -0,0 +1,43 @@
+InitialiseDashboardClean <- function(){
+  if (Sys.getenv("RPROJ") != "") {
+        assign("RPROJ", list(PROJHOME = Sys.getenv("RPROJ")), envir = globalenv())
+        setwd(RPROJ$PROJHOME)
+  }
+  PROJNAME <- rev(stringr::str_split(RPROJ$PROJHOME,"/")[[1]])[1]
+  PROJSTUB <- gsub(paste0("cleaning/",PROJNAME),"",RPROJ$PROJHOME)
+   assign("RPROJ", list(PROJHOME = RPROJ$PROJHOME, 
+                        PROJNAME = PROJNAME,
+                        PROJSTUB = PROJSTUB), 
+            envir = globalenv())
+   
+   fileSources = file.path("code", list.files("code", pattern = "*.[rR]$"))
+    sapply(fileSources, source, .GlobalEnv)
+}
+
+DashboardFolder <- function(inside="data_raw",f=NULL){
+  retVal <- paste0(RPROJ$PROJSTUB,inside,"/",RPROJ$PROJNAME,"/")
+  if(!is.null(f)){
+    retVal <- paste0(retVal,f)
+  }
+  return(retVal)
+}
+
+DashboardEmail <- function(emailBCC,emailSubject,emailText){
+  emails <- readxl::read_excel("/etc/gmailr/emails.xlsx")
+  emails <- na.omit(emails[[emailBCC]])
+  
+  mime() %>%
+    to("dashboards@fhi.no") %>%
+    from("Dashboards FHI <dashboardsfhi@gmail.com>") %>%
+    bcc(paste0(emails,collapse=",")) %>%
+    subject(emailSubject) %>%
+    text_body(emailText) -> text_msg
+  
+  currentWD <- getwd()
+  tmp <- tempdir()
+  file.copy("/etc/gmailr/.httr-oauth",paste0(tmp,"/.httr-oauth"))
+  setwd(tmp)
+  gmail_auth()
+  send_message(text_msg)
+  setwd(currentWD)
+}
