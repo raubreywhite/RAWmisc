8ad7bee97b063cabf7f1fa0fdc932e099506c214
Richard White <r.aubrey.white@gmail.com>
2017-05-11 14:54:02 +0200

    Importing data.table

--- R/datatable.R
+++ R/datatable.R
@@ -2,9 +2,15 @@
 #' @param d a
 #' @param switch a vector coded like: c("old"="new")
 #' @param var a
+#' @import data.table
 #' @export RecodeDT
 RecodeDT <- function(d,switch,var){
-  switch <- data.table::data.table(orig=names(switch),replace=switch)
+  if(is.numeric(d[[var]])){
+    switch <- data.table::data.table(orig=as.numeric(names(switch)),replace=as.numeric(switch))
+  } else {
+    switch <- data.table::data.table(orig=names(switch),replace=switch)
+  }
+
   txt <- sprintf("d[switch, on=.(%s=orig), %s:=replace]",var,var)
   eval(parse(text=txt))
 }
