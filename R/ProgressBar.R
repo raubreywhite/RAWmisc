a6f11bae77164c81e3c30dc8e275a50cdacdde7e
Richard White <a@rwhite.no>
2017-07-14 11:00:33 +0200

    d

--- R/ProgressBar.R
+++ R/ProgressBar.R
@@ -33,15 +33,14 @@ ProgressBarSet <- function(pb, value){
   }
   timeLeft <- timeTotal-timeTaken
 
-  #retval <- sprintf("\r%s Total time: %s min. %s%% completed in %s min at %s. %s min remaining.",
-  #            format(Sys.time(),'%H:%M:%S'),
-  #            Format(timeTotal,digits=1),
-  #            Format(propCompleted*100,digits=0),
-  #            Format(timeTaken,digits=1),
-  #            "hi",
-  #            Format(timeLeft,digits=1)
-  #            )
-  retval <- "ok"
+  retval <- sprintf("\r%s Total time: %s min. %s%% completed in %s min at %s. %s min remaining.",
+              format(Sys.time(),'%H:%M:%S'),
+              Format(timeTotal,digits=1),
+              Format(propCompleted*100,digits=0),
+              Format(timeTaken,digits=1),
+              "hi",
+              Format(timeLeft,digits=1)
+              )
   if(pb$flush){
     cat(retval)
     utils::flush.console()
