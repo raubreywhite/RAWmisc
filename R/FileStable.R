4dcfaff4bb4ac267b7c2f5bea852f1e017f5f4d5
Richard White <a@rwhite.no>
2017-07-26 15:36:53 +0200

    IsFileStable

--- R/FileStable.R
+++ R/FileStable.R
@@ -1,9 +1,9 @@
-#' FileStable
+#' IsFileStable
 #' This determines if the file is stable (size not increasing/decreasing)
 #' @param file The file of interest
 #' @param delay Number of seconds between checking file size a second time
 #' @export FileStable
-FileStable <- function(file=tempfile(), delay=1){
+IsFileStable <- function(file=tempfile(), delay=1){
   if (file.exists(file)){
     size1 <- file.info(file)$size[1]
     Sys.sleep(delay)
