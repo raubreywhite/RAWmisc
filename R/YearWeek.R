10ff5f964d72da41eca14a7b8516c99002df8a0e
Richard White <r@rwhite.no>
2018-03-20 13:59:39 +0200

    Fixing YearWeek

--- R/YearWeek.R
+++ R/YearWeek.R
@@ -4,5 +4,5 @@
 #' @import data.table
 #' @export YearWeek
 YearWeek <- function(date=lubridate::today()){
-  return(sprintf("%s-%s",data.table::year(date),data.table::isoweek(date)))
+  return(sprintf("%s-%s",data.table::year(date),formatC(data.table::isoweek(date),width=2,flag="0")))
 }
