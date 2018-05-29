f87497ca93595d05d49369c878e6f777a0fd4cee
Richard White <r@rwhite.no>
2018-05-29 08:22:52 +0200

    FileTimeModified

--- R/FileStable.R
+++ R/FileStable.R
@@ -13,35 +13,11 @@ IsFileStable <- function(file=tempfile(), delay=5){
   return(FALSE)
 }
 
-#' IsFileChanged
+#' FileTimeModified
 #' This determines if the file has been changed (checks modification times)
-#' @param fileToCheck The file of interest
-#' @param fileDetails File that contains the details of things being checked
-#' @param delay Number of seconds between checking file size a second time
-#' @export IsFileChanged
-IsFileChanged <- function(fileToCheck=tempfile(), fileDetails=tempfile(), delay=5){
-  if(!file.exists(fileToCheck)) return(FALSE)
-
-  lastMod <- file.info(fileToCheck)$mtime
-
-  if(!file.exists(fileDetails)){
-    saveRDS(lastMod,fileDetails)
-    return(TRUE) # first time we have checked the file
-  } else {
-    recordedMod <- readRDS(fileDetails)
-    if(recordedMod==lastMod){
-      # file hasn't changed
-      return(FALSE)
-    } else {
-      if(RAWmisc::IsFileStable(fileToCheck, delay=delay)){
-        # file has changed but is unstable, cant be trusted
-        return(FALSE)
-      } else {
-        # file has changed and is stable
-        saveRDS(lastMod,fileDetails)
-        return(TRUE)
-      }
-    }
-  }
+#' @param file The file of interest
+#' @export FileTimeModified
+FileTimeModified <- function(file=tempfile()){
+  return(file.info(file)$mtime)
 }
 
