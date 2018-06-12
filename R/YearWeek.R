d03f9e8d59372dff1aff71ce6703876e1bcab3c5
Richard White <r@rwhite.no>
2018-06-12 12:12:04 +0300

    YearWeek

--- R/YearWeek.R
+++ R/YearWeek.R
@@ -1,7 +1,36 @@
+#' Year
+#' @param date The date of interest
+#' @importFrom lubridate today
+#' @export Year
+Year <- function(date=lubridate::today()){
+  yr <- format.Date(date,"%G")
+  return(yr)
+}
+
+#' WeekC
+#' @param date The date of interest
+#' @importFrom lubridate today
+#' @export WeekC
+WeekC <- function(date=lubridate::today()){
+  wk <- as.numeric(format.Date(date,"%V"))
+  wk <- formatC(wk, flag = "0", width = 2)
+  return(wk)
+}
+
+#' WeekN
+#' @param date The date of interest
+#' @importFrom lubridate today
+#' @export WeekN
+WeekN <- function(date=lubridate::today()){
+  wk <- as.numeric(format.Date(date,"%V"))
+  return(wk)
+}
+
 #' YearWeek
 #' @param date The date of interest
 #' @importFrom lubridate today
 #' @export YearWeek
 YearWeek <- function(date=lubridate::today()){
-  return(sprintf("%s-%s",format.Date(date,"%G"),format.Date(date,"%V")))
+  return(sprintf("%s-%s",Year(date),WeekC(date)))
 }
+
